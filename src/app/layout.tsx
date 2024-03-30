3e4be230a378d9bc8fe4f8af541c809fa4a39436
youserstack <youserstack@gmail.com>
2024-03-30 22:29:39 +0900

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-      </head>
       <body>
         <Header />
         {children}
