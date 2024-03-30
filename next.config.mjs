109a96e63230d8838a9bac82bdbe19857ddf716d
youserstack <youserstack@gmail.com>
2024-03-30 22:58:05 +0900

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
+    BASE_URL: "https://open-data-next-app.vercel.app/",
+    NEXT_PUBLIC_ENV: process.env.NEXT_PUBLIC_ENV,
     SEOUL_OPEN_URL: process.env.SEOUL_OPEN_URL,
     SEOUL_OPEN_API_KEY: process.env.SEOUL_OPEN_API_KEY,
   },
   images: {
     domains: ["res.cloudinary.com"],
   },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/about',
-  //       destination: `${process.env.SEOUL_OPEN_URL}`,
-  //     },
-  //   ]
-  // },
-  async headers() {
+  async rewrites() {
     return [
       {
         source: "/:path*",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "upgrade-insecure-requests",
-          },
-        ],
+        destination: `${process.env.SEOUL_OPEN_URL}/${process.env.SEOUL_OPEN_API_KEY}/json/VwsmAdstrdNcmCnsmpW/:start/:end`,
       },
     ];
   },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/:path*",
+  //       headers: [
+  //         {
+  //           key: "Content-Security-Policy",
+  //           value: "upgrade-insecure-requests",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
--- src/app/open-data/page.tsx
+++ src/app/open-data/page.tsx
@@ -11,6 +11,7 @@ import "../../styles/open-data.scss";
 const ITEMS_PER_PAGE = 50; // 페이지당 아이템수
 const baseUrl =
   process.env.NODE_ENV === "production" ? process.env.BASE_URL : process.env.NEXT_PUBLIC_ENV;
+console.log({ next: process.env.NEXT_PUBLIC_ENV });
 
 export default function OpenData() {
   const [items, setItems] = useState([]);
@@ -45,9 +46,12 @@ export default function OpenData() {
       const start = ITEMS_PER_PAGE * (currentPage - 1) + 1; // 시작에 1을 더해준다.
       const end = ITEMS_PER_PAGE * currentPage;
       const openApiUrl = `${domain}/${authKey}/json/${serviceCode}/${start}/${end}`;
-      const data: any = await getDataWithOpenApi({
-        url: openApiUrl,
-      });
+      // const openApiUrl = `${baseUrl}/api/${authKey}/json/${serviceCode}/${start}/${end}`;
+      console.log({ openApiUrl });
+      const data = await fetch(openApiUrl).then((res) => res.json());
+      // const data: any = await getDataWithOpenApi({
+      //   url: openApiUrl,
+      // });
       const { RESULT, list_total_count, row } = data.VwsmAdstrdNcmCnsmpW;
       console.log({ data });
       const keys: any = Object.keys(data);
